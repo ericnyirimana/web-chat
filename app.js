eee3fadcde7215d52794cf937b84ed7390c95a29
Eric Nyirimana <ericnyirimana31@gmail.com>
2020-01-15 16:56:50 +0200

    WC-170723068-task(environment): restructure the codebase - Setup routes - create error handler for 404 status [Finishes WC-170723068]

---
+++ app.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import routes from './routes';
+
+const isProduction = process.env.NODE_ENV === 'production';
+const app = express();
+app.listen(3400, () => console.log(`Server is listening on port 3400`));
+app.use(
+  bodyParser.urlencoded({
+    limit: '500mb',
+    extended: false,
+    parameterLimit: 500000
+  })
+);
+app.use(bodyParser.json({ limit: '500mb' }));
+app.use(routes);
+// / catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+app.use((err, req, res, next) => {
+  let error = {};
+  if (!isProduction) {
+    error = err;
+  }
+  res.status(err.status || 500);
+  res.json({
+    errors: {
+      message: err.message,
+      error
+    }
+  });
+});
